e22b12e2ee8915be989de06fc43932277da5fb24
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-08 19:11:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,9 @@ def find_min_in_nested_arrays(src)
  row_index = 0 
  while row_index < src.count do 
    element_index = 0 
-   lowest_temperature = ""
+   lowest_temperature = 80
    while element_index < src[row_index].count do
-     if src[row_index][element_index] <= lowest_temperature
+     if src[row_index][element_index] < lowest_temperature
        lowest_temperature = src[row_index][element_index]
      end
      element_index += 1 
